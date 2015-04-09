c99983ebdfa232740c502d02fc9deaf08cfb0cb3
juclark96 <jcc2244@barnard.edu>
2015-04-09 23:41:07 +0000

    Lab Hubble's law

---
+++ .c9/metadata/preview-https:/lab-model-fitting-juclark96.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://lab-model-fitting-juclark96.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://lab-model-fitting-juclark96.c9.io","previewer":"preview.browser"},"timestamp":1428022578857,"hash":"67452301efcdab8998badcfe10325476c3d2e1f0"}
--- fitter.py
+++ fitter.py
@@ -1,105 +1,109 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 
-
-#  ------------------------------------------------------
 #  PLOTDATA: plots a line and saves
 #  it as a png image ...
 #
 def plotData(x,y,name):
-	plt.clf()
-	plt.plot(x,y,'k.')
-	plt.title('Raw Data')
-	plt.savefig(name)
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.title('Raw Data')
+    plt.savefig(name)
 
 #  ------------------------------------------------------
 #  PLOTFIT: plots line AND data and saves it as a png
 #
 #
 def plotFit(x,y,z,name):
-	plt.clf()
-	plt.plot(x,y,'k.')
-	plt.plot(x,z,'k-')
-	plt.title('Fit to Data')
-	plt.savefig(name)
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.plot(x,z,'k-')
+    plt.title('Fit to Data')
+    plt.savefig(name)
 
 #  ------------------------------------------------------
 #  GRABDATA
 #    Pulls data from file and stores it in x,y
 #
 def grabData():
-	f = open('linFit.dat','r')
-		
-	dataString = f.read()
-	f.close()
-	dataLines = dataString.split('\n');
-	N = len(dataLines)-1
-	x = []
-	y = []
+    dat=np.genfromtxt('linFit.txt')
+    x=dat[:,0]
+    y=dat[:,1]
 
-	for i in range(N):
-		line = dataLines[i].split('\t')
-		x.append(float( line[0] ))
-		y.append(float( line[1] ))
+    return (x,y)
 
-	return (x,y)
-	
 
 
 #  ------------------------------------------------------
-#  F
-#    Given data list x, returns a list y = mx+b 
-#
-def f(x,m,b):
-	return 0					# FIXME!
+# Step 3: Given list of x return list of h0*x
+def hubble(x,h0):
+    return [h0*i for i in x]
 
-#
-#   SSR
-# 
+
 #     Given data lists x and y, this function
-#     finds how well a line, f(x) = mx+b fits
+#     finds how well a line, hubble(x) = h0*x fits
 #     by calculating the sum of the square of
 #     the residuals
 #
-def SSR(x,y,m,b):
-	return 0 					# FIXME!
+
+#Step 5:
+def SSR(x,y,h0):
+    z=hubble(x,h0)
+    SSR=0
+    for i in range(len(x)):
+        residuals=(z[i]-y[i])**2 
+        SSR = SSR + residuals
+    return SSR
+    
+
 
 #
-#  STEP 3 ---- Get data, plot it
+#  Step 2: Get data, plot it
 #
 
+(x, y)=grabData()
+plotData(x,y,'rawData.png')
+
+#   Step 4: Plot example function
+#
+h0=10
+z=hubble(x,h0)
+print z
+plotFit(x,y,z,"badFit.png")
 
 #
-#  STEP 5 ---- Plot example function
+#  Step 6: Find SSR for poor fit from example function
 #
+h0=10
+f=SSR(x,y,h0)
+print f
 
 #
-#  STEP 7 ---- Find SSR for poor fit from example function
+#   list for possible slopes. 
 #
 
-#
-#  STEP 8 ---- Make lists for possible slopes and
-#              intercepts, then print them
-#
-
-slopes = []
-for i in range(-50,50):
-	slopes.append( i/10.0 )
+h0s=np.arange(0.,100.,0.1)
 
 #
-#  STEP 9 ---- Find slope and intercept that minimize
+#  Step 7: Find slope and intercept that minimize
 #              the RSS
 #
-bestSlope = 0
-bestInter = 0
-minSSR = 10000
+best_h0 = 0.
+min_ssr = np.inf
+for h0 in h0s:
+    g=SSR(x,y,h0)
+    if g < min_ssr:
+        min_ssr=g
+        best_h0=h0
+print best_h0
 
 #
-#  STEP 10 ---- Plot Result
+#   Step 8: Plot Result
 #
 #
 
 
 #
-#  STEP 11 ---- Plot Residuals
-#
-#
+#   Step 9: Plot Residuals
